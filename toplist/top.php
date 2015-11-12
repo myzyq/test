8177e219bc155932f51b7b7480c37c781b20ff3f
myzyq <myzyq0@163.com>
2015-11-12 09:48:42 +0800

    提交部署

---
+++ toplist/top.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/9/28 0028
+ * Time: ÏÂÎç 14:06
+ */
+
+namespace toplist;
+
+class top
+{
+    public $top;
+
+    public function setTop($top)
+    {
+        $this->top = $top;
+    }
+}
